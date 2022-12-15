261803cf4dbf784cffbcba7b258f29d42646c42e
doba16 <92383352+doba16@users.noreply.github.com>
2022-12-15 17:50:58 +0100

    fix: Added x flag for kubectl.

--- setup-kubectl.sh
+++ setup-kubectl.sh
@@ -4,6 +4,8 @@ curl -LO "https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/$KUBECTL_VERSION/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
 
+chmod +x kubectl
+
 mkdir $GITHUB_WORKSPACE/bin
 mv kubectl $GITHUB_WORKSPACE/bin
 echo "$GITHUB_WORKSPACE/bin" >> $GITHUB_PATH
